6969c9f6fb6b1cdce88fecd2d4dfbdc4b51cce5b
Sofia <msofiajmolima@gmail.com>
2026-04-24 13:55:34 -0300

    Correção Lista 2 recursão em listas

--- src/07.2-funcional-recursao-listas.kt
+++ src/07.2-funcional-recursao-listas.kt
@@ -70,7 +70,7 @@ fun main() {
      */
     fun separarParesImpares(lista: List<Int>): Pair<List<Int>, List<Int>> {
         tailrec fun pi(list: List<Int>, lpares: List<Int> = listOf(), limpares: List<Int> = listOf()): Pair<List<Int>, List<Int>>{
-            if(list.isEmpty()) return Pair(lpares, limpares)
+            if(list.isEmpty()) return Pair(limpares, lpares)
             if(list.first() % 2 == 0) return pi(list.drop(1), lpares + list.first(), limpares)
             return pi(list.drop(1), lpares,limpares + list.first(),)
         }
@@ -100,19 +100,20 @@ fun main() {
      * Dica: Para criar uma lista contendo N zeros, você pode usar `List(N) { 0 }`.
      */
     fun abastecimentoCasas(N: Int, caminhoes: List<Triple<Int, Int, Int>>): List<Int> {
-        tailrec fun abastecer(A: Int, B: Int, L: Int, casas: List<Int> = listOf()): List<Int>{
-            if(A > B) return casas
-            return abastecer(A+1, B, L, casas + L)
+        tailrec fun abastecer(A: Int, B: Int, L: Int, casas: List<Int>, acumulador: List<Int>, index: Int = 0): List<Int>{
+            if(index == casas.size) return acumulador
+            if(index !in A..B) return abastecer(A, B, L, casas, acumulador + listOf(casas[index]), index+1)
+            return abastecer(A, B, L, casas, acumulador + listOf(casas[index] + L), index+1)
         }
         tailrec fun abastecerTodas(caminhoes: List<Triple<Int, Int, Int>>, casas: List<Int>): List<Int>{
             if(caminhoes.isEmpty()) return casas
-            return abastecerTodas(caminhoes.drop(1), casas+abastecer(caminhoes.first().first, caminhoes.first().second, caminhoes.first().third))
+            return abastecerTodas(caminhoes.drop(1), abastecer(caminhoes.first().first, caminhoes.first().second, caminhoes.first().third, casas, listOf<Int>()))
         }
         return abastecerTodas(caminhoes, List(N){0})
     }
 
     // <INCLUA O TRECHO ABAIXO PARA TESTAR SUA SOLUÇÃO>
-    /*
+
     assertEquals("abastecimentoCasas(7, listOf(Triple(0, 2, 4), Triple(5, 6, 5)))", listOf(4, 4, 4, 0, 0, 5, 5), abastecimentoCasas(7, listOf(Triple(0, 2, 4), Triple(5, 6, 5))))
     assertEquals("abastecimentoCasas(5, listOf(Triple(0, 4, 50)))", listOf(50, 50, 50, 50, 50), abastecimentoCasas(5, listOf(Triple(0, 4, 50))))
     assertEquals("abastecimentoCasas(6, listOf(Triple(0, 5, 1), Triple(1, 5, 1), Triple(2, 5, 1), Triple(3, 5, 1), Triple(4, 5, 1), Triple(5, 5, 1)))", listOf(1, 2, 3, 4, 5, 6), abastecimentoCasas(6, listOf(Triple(0, 5, 1), Triple(1, 5, 1), Triple(2, 5, 1), Triple(3, 5, 1), Triple(4, 5, 1), Triple(5, 5, 1))))
@@ -120,5 +121,5 @@ fun main() {
     assertEquals("abastecimentoCasas(20, listOf(Triple(1, 19, 5), Triple(7, 12, 10), Triple(0, 3, 30), Triple(8, 19, 3), Triple(0, 2, 8), Triple(6, 9, 4)))", listOf(38, 43, 43, 35, 5, 5, 9, 19, 22, 22, 18, 18, 18, 8, 8, 8, 8, 8, 8, 8), abastecimentoCasas(20, listOf(Triple(1, 19, 5), Triple(7, 12, 10), Triple(0, 3, 30), Triple(8, 19, 3), Triple(0, 2, 8), Triple(6, 9, 4))))
     assertEquals("abastecimentoCasas(10, listOf(Triple(1, 3, 13), Triple(4, 5, 30), Triple(0, 3, 20), Triple(0, 0, 5), Triple(8, 9, 27), Triple(3, 8, 12), Triple(3, 9, 25), Triple(6, 7, 2), Triple(2, 3, 13), Triple(4, 7, 8), Triple(6, 6, 7), Triple(1, 5, 4), Triple(4, 8, 2), Triple(5, 9, 17), Triple(0, 3, 8)))", listOf(33, 45, 58, 95, 81, 98, 73, 66, 83, 69), abastecimentoCasas(10, listOf(Triple(1, 3, 13), Triple(4, 5, 30), Triple(0, 3, 20), Triple(0, 0, 5), Triple(8, 9, 27), Triple(3, 8, 12), Triple(3, 9, 25), Triple(6, 7, 2), Triple(2, 3, 13), Triple(4, 7, 8), Triple(6, 6, 7), Triple(1, 5, 4), Triple(4, 8, 2), Triple(5, 9, 17), Triple(0, 3, 8))))
     println("Todos os testes passaram para a função abastecimentoCasas!")
-    */
+
 }
